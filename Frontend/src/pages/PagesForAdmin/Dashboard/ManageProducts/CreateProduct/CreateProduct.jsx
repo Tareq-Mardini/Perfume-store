8fdcca091879b8da3c51714bebbb83658055b817
Tareq-Mardini <tareqmardini25@gmail.com>
2026-04-21 16:07:10 +0300

    some edit in page Create Products to admin

--- Frontend/src/pages/PagesForAdmin/Dashboard/ManageProducts/CreateProduct/CreateProduct.jsx
+++ Frontend/src/pages/PagesForAdmin/Dashboard/ManageProducts/CreateProduct/CreateProduct.jsx
@@ -4,6 +4,7 @@ import {
   uploadProductImages,
 } from "../../../../../services/productsService.js";
 import "./CreateProduct.css";
+import { Link } from "react-router-dom";
 
 export default function CreateProduct() {
   const [form, setForm] = useState({
@@ -94,143 +95,156 @@ export default function CreateProduct() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="create-product-page">
-      <h2>Create Product</h2>
+    <>
+      <Link to="/admin/products">
+        <button className="btn-create-product">Create New Product</button>
+      </Link>
+      <form onSubmit={handleSubmit} className="create-product-page">
+        <h2>Create Product</h2>
 
-      {/* 🔹 Basic Info */}
-      <div className="grid-2">
-        <div className="form-group">
-          <label>Price</label>
-          <input name="price" value={form.price} onChange={handleChange} />
-          {errors.price && <p className="error">{errors.price[0]}</p>}
-        </div>
-
-        <div className="form-group">
-          <label>Price Before Discount</label>
-          <input
-            name="price_before_discount"
-            value={form.price_before_discount}
-            onChange={handleChange}
-          />
-          {errors.price_before_discount && (
-            <p className="error">{errors.price_before_discount[0]}</p>
-          )}
-        </div>
-
-        <div className="form-group">
-          <label>Bottle Volume</label>
-          <input
-            name="bottle_volume"
-            value={form.bottle_volume}
-            onChange={handleChange}
-          />
-          {errors.bottle_volume && (
-            <p className="error">{errors.bottle_volume[0]}</p>
-          )}
-        </div>
-
-        <div className="form-group">
-          <label>Category</label>
-          <select name="category" value={form.category} onChange={handleChange}>
-            <option value="unisex">unisex</option>
-            <option value="men">men</option>
-            <option value="women">women</option>
-          </select>
-          {errors.category && <p className="error">{errors.category[0]}</p>}
-        </div>
-      </div>
-
-      {/* 🌍 Translations */}
-      {form.translations.map((t, index) => (
-        <div key={index} className="translation-box">
-          <h3>{t.language_code.toUpperCase()}</h3>
-
-          <div className="grid-2">
-            <div className="form-group">
-              <label>Name</label>
-              <input
-                name="name"
-                value={t.name}
-                onChange={(e) => handleTranslationChange(index, e)}
-              />
-              {errors?.translations?.[index]?.name?.[0] && (
-                <p className="error">{errors.translations[index].name[0]}</p>
-              )}
-            </div>
-
-            <div className="form-group">
-              <label>Character</label>
-              <input
-                name="character"
-                value={t.character}
-                onChange={(e) => handleTranslationChange(index, e)}
-              />
-              {errors?.translations?.[index]?.character?.[0] && (
-                <p className="error">
-                  {errors.translations[index].character[0]}
-                </p>
-              )}
-            </div>
-
-            <div className="form-group">
-              <label>Sillage</label>
-              <input
-                name="sillage"
-                value={t.sillage}
-                onChange={(e) => handleTranslationChange(index, e)}
-              />
-              {errors?.translations?.[index]?.sillage?.[0] && (
-                <p className="error">{errors.translations[index].sillage[0]}</p>
-              )}
-            </div>
-
-            <div className="form-group">
-              <label>Longevity</label>
-              <input
-                name="longevity"
-                value={t.longevity}
-                onChange={(e) => handleTranslationChange(index, e)}
-              />
-              {errors?.translations?.[index]?.longevity?.[0] && (
-                <p className="error">
-                  {errors.translations[index].longevity[0]}
-                </p>
-              )}
-            </div>
+        {/* 🔹 Basic Info */}
+        <div className="grid-2">
+          <div className="form-group">
+            <label>Price</label>
+            <input name="price" value={form.price} onChange={handleChange} />
+            {errors.price && <p className="error">{errors.price[0]}</p>}
           </div>
 
           <div className="form-group">
-            <label>Description</label>
-            <textarea
-              name="description"
-              value={t.description}
-              onChange={(e) => handleTranslationChange(index, e)}
+            <label>Price Before Discount</label>
+            <input
+              name="price_before_discount"
+              value={form.price_before_discount}
+              onChange={handleChange}
             />
-            {errors?.translations?.[index]?.description?.[0] && (
-              <p className="error">
-                {errors.translations[index].description[0]}
-              </p>
+            {errors.price_before_discount && (
+              <p className="error">{errors.price_before_discount[0]}</p>
             )}
           </div>
+
+          <div className="form-group">
+            <label>Bottle Volume</label>
+            <input
+              name="bottle_volume"
+              value={form.bottle_volume}
+              onChange={handleChange}
+            />
+            {errors.bottle_volume && (
+              <p className="error">{errors.bottle_volume[0]}</p>
+            )}
+          </div>
+
+          <div className="form-group">
+            <label>Category</label>
+            <select
+              name="category"
+              value={form.category}
+              onChange={handleChange}
+            >
+              <option value=""></option>
+              <option value="unisex">unisex</option>
+              <option value="men">men</option>
+              <option value="women">women</option>
+            </select>
+            {errors.category && <p className="error">{errors.category[0]}</p>}
+          </div>
         </div>
-      ))}
 
-      {/* 📸 Upload */}
-      <div className="upload-box">
-        <label>Upload Images</label>
-        <input type="file" multiple onChange={handleImagesChange} />
-      </div>
+        {/* 🌍 Translations */}
+        {form.translations.map((t, index) => (
+          <div key={index} className="translation-box">
+            <h3>{t.language_code.toUpperCase()}</h3>
 
-      {/* 🖼️ Preview */}
-      <div className="preview">
-        {images.map((img, i) => (
-          <img key={i} src={URL.createObjectURL(img)} alt="preview" />
+            <div className="grid-2">
+              <div className="form-group">
+                <label>Name</label>
+                <input
+                  name="name"
+                  value={t.name}
+                  onChange={(e) => handleTranslationChange(index, e)}
+                />
+                {errors?.translations?.[index]?.name?.[0] && (
+                  <p className="error">{errors.translations[index].name[0]}</p>
+                )}
+              </div>
+
+              <div className="form-group">
+                <label>Character</label>
+                <input
+                  name="character"
+                  value={t.character}
+                  onChange={(e) => handleTranslationChange(index, e)}
+                />
+                {errors?.translations?.[index]?.character?.[0] && (
+                  <p className="error">
+                    {errors.translations[index].character[0]}
+                  </p>
+                )}
+              </div>
+
+              <div className="form-group">
+                <label>Sillage</label>
+                <input
+                  name="sillage"
+                  value={t.sillage}
+                  onChange={(e) => handleTranslationChange(index, e)}
+                />
+                {errors?.translations?.[index]?.sillage?.[0] && (
+                  <p className="error">
+                    {errors.translations[index].sillage[0]}
+                  </p>
+                )}
+              </div>
+
+              <div className="form-group">
+                <label>Longevity</label>
+                <input
+                  name="longevity"
+                  value={t.longevity}
+                  onChange={(e) => handleTranslationChange(index, e)}
+                />
+                {errors?.translations?.[index]?.longevity?.[0] && (
+                  <p className="error">
+                    {errors.translations[index].longevity[0]}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label>Description</label>
+              <textarea
+                style={{ maxWidth: "100%" }}
+                name="description"
+                value={t.description}
+                onChange={(e) => handleTranslationChange(index, e)}
+              />
+              {errors?.translations?.[index]?.description?.[0] && (
+                <p className="error">
+                  {errors.translations[index].description[0]}
+                </p>
+              )}
+            </div>
+          </div>
         ))}
-      </div>
 
-      <button type="submit" disabled={loading}>
-        {loading ? "Creating..." : "Create Product"}
-      </button>
-    </form>
+        {/* 📸 Upload */}
+        <div className="upload-box">
+          <label>Upload Images</label>
+          <input type="file" multiple onChange={handleImagesChange} />
+        </div>
+
+        {/* 🖼️ Preview */}
+        <div className="preview">
+          {images.map((img, i) => (
+            <img key={i} src={URL.createObjectURL(img)} alt="preview" />
+          ))}
+        </div>
+
+        <button type="submit" disabled={loading}>
+          {loading ? "Creating..." : "Create Product"}
+        </button>
+      </form>
+    </>
   );
 }
--- Frontend/src/pages/PagesForAdmin/Dashboard/ManageProducts/DisplayProducts/Products.jsx
+++ Frontend/src/pages/PagesForAdmin/Dashboard/ManageProducts/DisplayProducts/Products.jsx
@@ -5,6 +5,7 @@ import {
   getProducts,
   deleteProduct,
 } from "../../../../../services/productsService";
+import { Link } from "react-router-dom";
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState("");
@@ -42,58 +43,63 @@ export default function Products() {
   };
 
   return (
-    <div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th className="th">Name</th>
-            <th className="th">Image</th>
-            <th className="th">Actions</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <td className="td" data-label="Name">
-                {product.translations[1].name}
-              </td>
-              <td className="td" data-label="Image">
-                <img
-                  src={product.images[0]?.image}
-                  alt={product.name}
-                  width="50"
-                  height="50"
-                  style={{
-                    borderRadius: "8px",
-                    width: "130px",
-                    height: "100px",
-                    margin: "auto",
-                  }}
-                />
-              </td>
-
-              <td className="td" data-label="Actions">
-                <button
-                  className="btn-Dash btn-view"
-                  onClick={() =>
-                    navigate(`/admin/product-Detail/${product.id}`)
-                  }
-                >
-                  View
-                </button>
-                <button className="btn-Dash btn-edit">Edit</button>
-                <button
-                  className="btn-Dash btn-delete"
-                  onClick={() => handleDelete(product.id)}
-                >
-                  Delete
-                </button>
-              </td>
+    <>
+      <Link to="/admin/create-product">
+        <button className="btn-create-product">Create New Product</button>
+      </Link>
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th className="th">Name</th>
+              <th className="th">Image</th>
+              <th className="th">Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td className="td" data-label="Name">
+                  {product.translations[1].name}
+                </td>
+                <td className="td" data-label="Image">
+                  <img
+                    src={product.images[0]?.image}
+                    alt={product.name}
+                    width="50"
+                    height="50"
+                    style={{
+                      borderRadius: "8px",
+                      width: "130px",
+                      height: "100px",
+                      margin: "auto",
+                    }}
+                  />
+                </td>
+
+                <td className="td" data-label="Actions">
+                  <button
+                    className="btn-Dash btn-view"
+                    onClick={() =>
+                      navigate(`/admin/product-Detail/${product.id}`)
+                    }
+                  >
+                    View
+                  </button>
+                  <button className="btn-Dash btn-edit">Edit</button>
+                  <button
+                    className="btn-Dash btn-delete"
+                    onClick={() => handleDelete(product.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
