c76e1cdffbfd71ffc8748fe2af09eb25b1ef2696
Tareq-Mardini <tareqmardini25@gmail.com>
2026-04-26 10:31:25 +0300

    The site has been implemented with an English language font and an Arabic language font

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles/rootStyle.css";
 import HomePage from "./pages/PagesForClient/Home/HomePage";
 import AdminLogin from "./pages/PagesForAdmin/Login/LoginPage";
 import ProductPage from "./pages/PagesForClient/Products/ProductsPage";
@@ -13,39 +14,48 @@ import DisplayDetailProduct from "./pages/PagesForAdmin/Dashboard/ManageProducts
 import CreateProduct from "./pages/PagesForAdmin/Dashboard/ManageProducts/CreateProduct/CreateProduct";
 import ManageOrders from "./pages/PagesForAdmin/Dashboard/ManageOrders/ManageOrders";
 import { LanguageProvider } from "./Context/LanguageContext";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./i18n";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Cairo"],
+  },
+});
 function App() {
   return (
     <>
-      <LanguageProvider>
-        <BrowserRouter>
-          <CartProvider>
-            <Routes>
-              {/* routes to client */}
-              <Route path="/" element={<HomePage />} />
-              <Route path="/products" element={<ProductPage />} />
-              <Route path="/product/:id" element={<SpecificProductPage />} />
-              <Route path="/products/Checkout" element={<Checkout />} />
-              <Route path="/terms" element={<Terms />} />
-              <Route path="/about" element={<AboutUs />} />
-              {/* routes to admin */}
-              <Route element={<DashboardLayout />}>
-                <Route path="/admin/products" element={<Products />} />
-                <Route
-                  path="/admin/products/product-Detail/:id"
-                  element={<DisplayDetailProduct />}
-                />
-                <Route
-                  path="/admin/products/create-product"
-                  element={<CreateProduct />}
-                />
-                <Route path="/admin/orders" element={<ManageOrders />} />
-              </Route>
-              <Route path="/login-admin" element={<AdminLogin />} />
-            </Routes>
-          </CartProvider>
-        </BrowserRouter>{" "}
-      </LanguageProvider>
+      <ThemeProvider theme={theme}>
+        <LanguageProvider>
+          <BrowserRouter>
+            <CartProvider>
+              <Routes>
+                {/* routes to client */}
+                <Route path="/" element={<HomePage />} />
+                <Route path="/products" element={<ProductPage />} />
+                <Route path="/product/:id" element={<SpecificProductPage />} />
+                <Route path="/products/Checkout" element={<Checkout />} />
+                <Route path="/terms" element={<Terms />} />
+                <Route path="/about" element={<AboutUs />} />
+                {/* routes to admin */}
+                <Route element={<DashboardLayout />}>
+                  <Route path="/admin/products" element={<Products />} />
+                  <Route
+                    path="/admin/products/product-Detail/:id"
+                    element={<DisplayDetailProduct />}
+                  />
+                  <Route
+                    path="/admin/products/create-product"
+                    element={<CreateProduct />}
+                  />
+                  <Route path="/admin/orders" element={<ManageOrders />} />
+                </Route>
+                <Route path="/login-admin" element={<AdminLogin />} />
+              </Routes>
+            </CartProvider>
+          </BrowserRouter>{" "}
+        </LanguageProvider>
+      </ThemeProvider>
     </>
   );
 }
